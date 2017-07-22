00c877dc83d457df8c3f5d43555f651ee46e9eed
The Legendary Josh Wulf™ - As Seen on the Internet® <josh@magikcraft.io>
2017-07-22 19:30:33 +1000

    ES5

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = {
     spells: {
-        _default: () => magikcraft.io.dixit('Hello world'),
-        jump: () => magikcraft.io.exsultus(100)
+        _default: function () { magikcraft.io.dixit('Hello world')},
+        jump: function () { magikcraft.io.exsultus(100); }
     }
 };
